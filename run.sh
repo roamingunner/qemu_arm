12faa2ac26194955d466a8eb674f2570c9f46df2
Leo <103922926@qq.com>
2021-03-28 11:32:27 +0000

    add 9pfs to share host directory

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-arm -machine vexpress-a9 -dtb vexpress-v2p-ca9.dtb -kernel zImage -sd rootfs.ext2  -append "root=/dev/mmcblk0 rw console=ttyAMA0" --nographic -net nic -net tap,ifname=tap0
+sudo qemu-system-arm -machine vexpress-a9 -dtb vexpress-v2p-ca9.dtb -kernel zImage -sd rootfs.ext2  -append "root=/dev/mmcblk0 rw console=ttyAMA0" --nographic -fsdev local,security_model=passthrough,id=fsdev0,path=`pwd`/share -device virtio-9p-device,fsdev=fsdev0,mount_tag=hostshare -net nic -net tap,ifname=tap0
